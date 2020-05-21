4e9d5a37ad197907d946d3a43f9d25119165949d
SKRaja88 <sathishtprl@gmail.com>
2020-05-21 18:22:44 +0200

    springboot starter webapp first commit

---
+++ src/main/java/com/springG/Springboot/starter/SpringbootStarterApplication.java
@@ -0,0 +1,20 @@
+package com.springG.Springboot.starter;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootStarterApplication  {
+
+	private static final Logger log =   LoggerFactory.getLogger(SpringbootStarterApplication.class);
+	public static void main(String[] args) {
+log.info("***********************************************************************");
+		SpringApplication.run(SpringbootStarterApplication.class, args);
+
+
+
+	}
+
+}
